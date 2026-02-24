d93730ac38d9240020db4a6a0c9ea937e0766d5f
Helen Andrade <helenandradeoliveira21@gmail.com>
2026-02-24 14:24:00 -0300

    feat: componentizando

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx'],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'max-content 1fr max-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
-
       maxWidth: {
         app: '700px',
-      },
-
-      colors: {
-        helen: '#333',
       },
     },
   },
