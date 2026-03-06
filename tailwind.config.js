1639b0a130aafc9ae05e6d2bc273f1791b9ba20d
Helen Andrade <helenandradeoliveira21@gmail.com>
2026-03-06 16:34:40 -0300

    feat: add variantes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       gridTemplateColumns: {
@@ -12,10 +8,36 @@ module.exports = {
         profile: 'max-content 1fr max-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
+
+      borderWidth: {
+        6: '6px',
+      },
+
+      colors: {
+        violet: {
+          25: '#fcfaff',
+        },
+
+        error: {
+          25: '#FFFBFA',
+          50: '#FEF3F2',
+          100: '#FEE4E2',
+          200: '#FECDCA',
+          300: '#FDA29B',
+          400: '#F97066',
+          500: '#F04438',
+          600: '#D92D20',
+          700: '#B42318',
+          800: '#912018',
+          900: '#7A271A',
+        },
+      },
+
       maxWidth: {
         app: '700px',
       },
-       keyframes: {
+
+      keyframes: {
         slideDownAndFade: {
           from: { opacity: 0, transform: 'translateY(-2px)' },
           to: { opacity: 1, transform: 'translateY(0)' },
